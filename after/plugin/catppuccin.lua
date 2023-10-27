b9b1869842f90883a6c7b2c96ef36418e6966e36
Tadeu Rangel <trangel.dev@gmail.com>
2023-10-27 07:57:31 -0700

    chore: change theme to catppuccin

---
+++ after/plugin/catppuccin.lua
@@ -0,0 +1,6 @@
+require("catppuccin").setup({
+    flavour = "mocha", -- latte, frappe, macchiato, mocha
+    transparent_background = true, -- disables setting the background color.
+})
+
+vim.cmd.colorscheme "catppuccin"
--- lua/rangeltadeu/packer.lua
+++ lua/rangeltadeu/packer.lua
@@ -7,73 +7,73 @@ return require('packer').startup(function(use)
     -- Packer can manage itself
     use 'wbthomason/packer.nvim'
 
-      use {
-	  'nvim-telescope/telescope.nvim', tag = '0.1.4',
-	  -- or                            , branch = '0.1.x',
+    use {
+        'nvim-telescope/telescope.nvim', tag = '0.1.4',
+        -- or                            , branch = '0.1.x',
 
-      requires = { {'nvim-lua/plenary.nvim'}, { "nvim-telescope/telescope-live-grep-args.nvim" } },
-      config = function()
-          require("telescope").load_extension("live_grep_args")
-      end
-  }
+        requires = { {'nvim-lua/plenary.nvim'}, { "nvim-telescope/telescope-live-grep-args.nvim" } },
+        config = function()
+            require("telescope").load_extension("live_grep_args")
+        end
+    }
 
-  use({ 'rose-pine/neovim', as = 'rose-pine' })
+    use { "catppuccin/nvim", as = "catppuccin" }
 
-  use({'nvim-treesitter/nvim-treesitter'}, {run = ':TSUpdate'})
+    use({'nvim-treesitter/nvim-treesitter'}, {run = ':TSUpdate'})
 
-  use('theprimeagen/harpoon')
+    use('theprimeagen/harpoon')
 
-  use('mbbill/undotree')
+    use('mbbill/undotree')
 
-  use('tpope/vim-fugitive')
+    use('tpope/vim-fugitive')
 
-  use {
-      'VonHeikemen/lsp-zero.nvim',
-      branch = 'v3.x',
-      requires = {
-          --- Uncomment these if you want to manage LSP servers from neovim
-          {'neovim/nvim-lspconfig'},
-          {'williamboman/mason.nvim'},
-          {'williamboman/mason-lspconfig.nvim'},
+    use {
+        'VonHeikemen/lsp-zero.nvim',
+        branch = 'v3.x',
+        requires = {
+            --- Uncomment these if you want to manage LSP servers from neovim
+            {'neovim/nvim-lspconfig'},
+            {'williamboman/mason.nvim'},
+            {'williamboman/mason-lspconfig.nvim'},
 
-          -- LSP Support
-          {'neovim/nvim-lspconfig'},
-          -- Autocompletion
-          {'hrsh7th/nvim-cmp'},
-          {'hrsh7th/cmp-nvim-lsp'},
-          {'L3MON4D3/LuaSnip'},
-          {'hrsh7th/cmp-buffer'},
-          {'hrsh7th/cmp-path'},
-          {'hrsh7th/cmp-nvim-lua'},
-          -- Snippets
-          {'L3MON4D3/LuaSnip'},
-          {'rafamadriz/friendly-snippets'},
+            -- LSP Support
+            {'neovim/nvim-lspconfig'},
+            -- Autocompletion
+            {'hrsh7th/nvim-cmp'},
+            {'hrsh7th/cmp-nvim-lsp'},
+            {'L3MON4D3/LuaSnip'},
+            {'hrsh7th/cmp-buffer'},
+            {'hrsh7th/cmp-path'},
+            {'hrsh7th/cmp-nvim-lua'},
+            -- Snippets
+            {'L3MON4D3/LuaSnip'},
+            {'rafamadriz/friendly-snippets'},
 
 
-      }
-  }
+        }
+    }
 
-  use("folke/zen-mode.nvim")
+    use("folke/zen-mode.nvim")
 
-  use {
-      "windwp/nvim-autopairs",
-      config = function() require("nvim-autopairs").setup {} end
-  }
+    use {
+        "windwp/nvim-autopairs",
+        config = function() require("nvim-autopairs").setup {} end
+    }
 
-  -- debugging
-  use("mfussenegger/nvim-dap")
-  use("leoluz/nvim-dap-go")
-  use("rcarriga/nvim-dap-ui")
-  use("theHamsta/nvim-dap-virtual-text")
-  use("nvim-telescope/telescope-dap.nvim")
+    -- debugging
+    use("mfussenegger/nvim-dap")
+    use("leoluz/nvim-dap-go")
+    use("rcarriga/nvim-dap-ui")
+    use("theHamsta/nvim-dap-virtual-text")
+    use("nvim-telescope/telescope-dap.nvim")
 
-  -- JS, TS
-  use { "mxsdev/nvim-dap-vscode-js", requires = {"mfussenegger/nvim-dap"} }
+    -- JS, TS
+    use { "mxsdev/nvim-dap-vscode-js", requires = {"mfussenegger/nvim-dap"} }
 
-  use {
-      "microsoft/vscode-js-debug",
-      opt = true,
-      run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out" 
-  }
+    use {
+        "microsoft/vscode-js-debug",
+        opt = true,
+        run = "npm install --legacy-peer-deps && npx gulp vsDebugServerBundle && mv dist out" 
+    }
 
 end)
