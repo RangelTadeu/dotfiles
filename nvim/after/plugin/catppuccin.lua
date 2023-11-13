297f07f5a64413514e2a5d78a871622e0869c2b9
Tadeu Rangel Chagas <tadeu.chagas@MAC-102341.local>
2023-11-12 16:56:09 -0800

    remove bk color setting  catppuccin

--- nvim/after/plugin/catppuccin.lua
+++ nvim/after/plugin/catppuccin.lua
@@ -1,6 +1,5 @@
 require("catppuccin").setup({
     flavour = "mocha", -- latte, frappe, macchiato, mocha
-    transparent_background = true, -- disables setting the background color.
 })
 
 vim.cmd.colorscheme "catppuccin"
